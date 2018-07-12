97ff883a60469eaafff1293f42b16c10eb52808a
Yo Wakita <wakita@gmail.com>
2018-07-11 21:57:21 -0700

    Adds interactive example of Hamming Distance

--- src/problems/hamming-distance/example.jsx
+++ src/problems/hamming-distance/example.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import solution from 'problems/hamming-distance/solution.md';
 import { padStart } from 'lodash';
+import { Button, Card, CardActions, CardHeader, CardContent } from '@material-ui/core';
+
+const getNumberStringLength = array => Math.max(...array.map(n => Math.floor(Math.log2(n))));
 
 const getBitArray = numberArray => {
   const binaries = numberArray.map(n => String(n.toString(2)));
-  const length = Math.max(...numberArray.map(n => Math.floor(Math.log2(n))));
+  const length = getNumberStringLength(numberArray);
   return binaries.map(b => ({
     int: parseInt(b, 2),
     binaryString: padStart(b, length + 1, 0)
@@ -12,6 +15,17 @@ const getBitArray = numberArray => {
       .join(' '),
   }));
 };
+
+const getXor = (x, y) => {
+  const length = getNumberStringLength([x, y]);
+  const xor = x ^ y;
+  const binaryString = padStart(xor.toString(2), length + 1, 0)
+    .split('')
+    .join(' ');
+  const replacedString = binaryString.replace(/1/g, '↑').replace(/0/g, ' ');
+  return replacedString;
+};
+
 class Example extends Component {
   constructor(props) {
     super(props);
@@ -26,6 +40,7 @@ class Example extends Component {
     x: 1,
     y: 4,
   };
+
   componentDidMount() {
     this.eval();
   }
@@ -41,45 +56,67 @@ class Example extends Component {
       });
   }
 
-  incrementX() {
-    this.setState({ x: this.state.x + 1 }, this.eval);
-  }
+  incrementX = () => this.setState({ x: this.state.x + 1 }, this.eval);
 
-  decrementX() {
-    this.setState({ x: this.state.x - 1 }, this.eval);
-  }
+  decrementX = () => this.setState({ x: this.state.x - 1 }, this.eval);
 
-  incrementY() {
-    this.setState({ y: this.state.y + 1 }, this.eval);
-  }
+  incrementY = () => this.setState({ y: this.state.y + 1 }, this.eval);
 
-  decrementY() {
-    this.setState({ y: this.state.y - 1 }, this.eval);
-  }
+  decrementY = () => this.setState({ y: this.state.y - 1 }, this.eval);
 
+  getMaxNumberStringLength = () => {
+    const { x, y } = this.state;
+    return Math.max(String(x).length, String(y).length);
+  };
   render() {
     const { output, x, y } = this.state;
     const max = Math.max(String(x).length, String(y).length);
     return (
-      <div>
-        <div>{`xInput: x = ${x}, y = ${y}`}</div>
-        <div>{`Output: ${output}`}</div>
-
-        <div>{'Explanation:'}</div>
-        <div>
-          {getBitArray([x, y]).map(b => (
-            <div data-length={b}>
-              {padStart(b.int, max, '0')}
-              {'- - - - '}
-              {b.binaryString}
-            </div>
-          ))}
-        </div>
-        <button onClick={this.incrementX}>Increment X</button>
-        <button onClick={this.decrementX}>Decrement X</button>
-        <button onClick={this.incrementY}>Increment Y</button>
-        <button onClick={this.decrementY}>Decrement Y</button>
-      </div>
+      <Card>
+        <CardHeader title="Interactive Hamming Distance Example" />
+        <CardContent>
+          <pre>
+            <code>
+              <div>{'Example: '}</div>
+              <div> </div>
+              <div>{`Input: x = ${x}, y = ${y}`}</div>
+              <div> </div>
+              <div>{`Output: ${output}`}</div>
+              <div> </div>
+              <div>{'Explanation:'}</div>
+              <div>
+                {getBitArray([x, y]).map(b => (
+                  <div>
+                    {padStart(b.int, this.getMaxNumberStringLength(), '0')}
+                    {' ( '}
+                    {b.binaryString}
+                    {' ) '}
+                  </div>
+                ))}
+                {padStart('', this.getMaxNumberStringLength() + 3, ' ')}
+                {getXor(x, y)}
+                <div>
+                  The above arrows point to positions where the corresponding bits are different.
+                </div>
+              </div>
+            </code>
+          </pre>
+        </CardContent>
+        <CardActions>
+          <Button size="small" variant="contained" color="primary" onClick={this.incrementX}>
+            X++
+          </Button>
+          <Button size="small" variant="contained" color="secondary" onClick={this.decrementX}>
+            X--
+          </Button>
+          <Button size="small" variant="contained" color="primary" onClick={this.incrementY}>
+            Y++
+          </Button>
+          <Button size="small" variant="contained" color="secondary" onClick={this.decrementY}>
+            Y--
+          </Button>
+        </CardActions>
+      </Card>
     );
   }
 }
